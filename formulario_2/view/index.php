4bae36d090e56123a3279ac22dc59f9bc2680c90
robertoleite.rl@gmail.com <robertoleite.rl@gmail.com>
2026-01-07 12:08:41 -0300

    VALIDATE FILTERS

--- formulario_2/view/index.php
+++ formulario_2/view/index.php
@@ -25,11 +25,11 @@
         }
         if (!$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)) {
             $erros[] = 'Digite um email válido';
-        } 
-        if ($nome == ''){
+        }
+        if ($nome == '') {
             $erros[] = 'Informe seu nome';
         }
-         if (empty($erros)) {
+        if (empty($erros)) {
             $mensagem = ($genero == 'M') ? "Seja Bem Vindo " : "Seja Bem Vinda";
             $mensagem .= " $nome !!!<br> Seu email é $email e sua idade é $idade";
         }
@@ -43,18 +43,12 @@
         <input type="text" class="form-control" name='email' value=''>
         <label class="form-label">IDADE: </label>
         <input type="number" class="form-control" name='idade' value='0'>
-        <label class="form-label">GENERO: </label>
-        <select name="genero" class="form-select" id="genero">
-            <option value="M">MASCULINO</option>
-            <option value="F">FEMININO</option>
-
-        </select>
         <div class="resultado">
-            <label class="form-label" id="total"><?php echo $mensagem?></label>
+            <label class="form-label" id="total"><?php echo $mensagem ?></label>
             <?php
             if (!empty($erros)) {
                 foreach ($erros as $erro) {
-                    echo "<li>$erro</li><br>";
+                    echo "<li>$erro</li>";
                 }
             }
             ?>
